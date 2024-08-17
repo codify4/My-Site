1ca012224170eae13276e480514210887bea880e
codify4 <kushta.joni@gmail.com>
2024-08-17 20:35:55 +0200

    List and socials added

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center p-24 bg-white">
+    <main className="flex flex-col items-center justify-center p-24 bg-black text-white w-full h-screen">
       <h1 className="text-5xl font-extrabold mb-5">my name is ijon</h1>
       <Image 
         src="/rarriPFP.jpg"
@@ -13,15 +13,35 @@ export default function Home() {
         className="rounded-full"
       />
 
-      <div className="w-3/4 text-xl text-center mt-5">
+      <div className="flex flex-col items-center justify-center w-3/4 text-xl text-center mt-5">
         <p>i am a full-stack swe building apps that may or may not make money.</p>
-        <p>i learned to code profficently in 8 months.</p>
-        <ul className="flex flex-col list-disc ml-5 mt-5">
+        <p>learned to code in 8 months.</p>
+        <ul className="flex flex-col items-center justify-center list-disc ml-5 mt-5 w-5/6 gap-1">
           <li>
-            My first app 
-            <Link href="https://trackr-gl.vercel.app/" className="font-bold underline text-blue-600 ml-1">Trackr.gl</Link>
+            built
+            <Link href="https://trackr-gl.framer.website/" className="font-bold underline text-blue-600 ml-1">Trackr.gl </Link>
+            to easily track your habits
+          </li>
+          <li>
+            doing a front-end internship
+          </li>
+          <li>
+            now learning to become an even better dev and business owner
+          </li>
+          <li>
+            cool stuff coming soon
           </li>
         </ul>
+
+        <div className="flex flex-col items-center justify-center gap-2 mt-5">
+          <h2 className="text-3xl font-bold mt-5">Connect with me</h2>
+          <div className="flex flex-row items-center justify-center gap-2">
+            <Link href="https://x.com/Ijon_k4" className="font-bold underline text-blue-600 ml-1">X(Twitter)</Link>
+            <Link href="https://github.com/codify4" className="font-bold underline text-blue-600 ml-1">Github</Link>
+            <Link href="https://www.linkedin.com/in/ijon-kushta-320b6831b/" className="font-bold underline text-blue-600 ml-1">Linkedin</Link>
+            <Link href="https://www.instagram.com/k_ijon4/" className="font-bold underline text-blue-600 ml-1">Instagram</Link>
+          </div>
+        </div>
       </div>
     </main>
   );
